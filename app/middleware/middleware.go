30297ab3579882c2a8e77cc0506a7b8120ac5a38
umutteroll <umut22erol@gmail.com>
2024-10-31 11:12:13 +0000

    chore : code design

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -14,30 +14,26 @@ import (
 var ctx = context.Background()
 
 func RateLimiter(c *fiber.Ctx) error {
+	ttl := time.Second * 60
+	limit := 5
+
+	userIP := c.IP()
+	key := "rate:" + userIP
+	rdb := helper.RedisClient()
+
+	requestCount, err := rdb.Get(ctx, key).Int()
+	if err == redis.Nil {
+		requestCount = 0
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Redis error" + err.Error())
+	}
+
+	if requestCount >= limit {
+		return c.Status(fiber.StatusTooManyRequests).SendString("Too many requests")
+	}
+
+	rdb.Incr(ctx, key)
+	rdb.Expire(ctx, key, ttl)
 	
-		ttl := time.Second * 60
-		limit := 5
-
-		userIP := c.IP()
-		key := "rate:" + userIP
-		rdb := helper.RedisClient()
-
-		requestCount, err := rdb.Get(ctx, key).Int()
-		if err == redis.Nil {
-			requestCount = 0
-		} else if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString("Redis error" + err.Error())
-		}
-
-		if requestCount >= limit {
-			return c.Status(fiber.StatusTooManyRequests).SendString("Too many requests")
-		}
-
-		rdb.Incr(ctx, key)
-		rdb.Expire(ctx, key, ttl)
-
-
-		
 	return c.Next()
-
 }
